d23b822ef114475a84800bae747c2a6475022163
Sai Charan <saicharankadiyala@gmail.com>
2024-12-28 07:02:53 +0530

    Updated todo route

--- models/todo-model.js
+++ models/todo-model.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const todoSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
     isDone: { type: Boolean, default: false },
     user: { type: mongoose.Schema.ObjectId, ref: "users", required: true },
   },
