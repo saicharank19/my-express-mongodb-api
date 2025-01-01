80792229d6cba90f946c54f6de877d5935b9068f
Sai Charan <saicharankadiyala@gmail.com>
2025-01-01 08:26:35 +0530

    Upted todo router to type script, id error need to be solved

--- routers/todo-route.ts
+++ routers/todo-route.ts
@@ -1,137 +1,170 @@
 import { todoModel } from "../models/todo-model.js";
 import dotenv from "dotenv";
 import { verifyJsonWebToken } from "./user-route.js";
-import express from "express";
+import express, { Request, Response } from "express";
+import { Todo, TitleList, Tasks } from "../types/todo.js";
 
 dotenv.config();
 
 const router = express.Router();
 
 //creates task
-router.post("/create", verifyJsonWebToken, async (req, res) => {
-  const userId = req.id;
-  const { title, description } = req.body;
-  const newTodo = new todoModel({ title, description, user: userId });
-  await newTodo.save();
-  return res.status(201).json({ message: "todo created successfully" });
-});
+router.post(
+  "/create",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const userId = req.id;
+    const { title } = req.body;
+    const newTodo = new todoModel({ title, user: userId });
+    await newTodo.save();
+    return res.status(201).json({ message: "todo created successfully" });
+  }
+);
 
 //changes the task status
-router.put("/update", verifyJsonWebToken, async (req, res) => {
-  const { todoId } = req.body;
-  const todoExists = await todoModel.findOne({ _id: todoId });
-  if (todoExists) {
-    todoExists.isDone = !todoExists.isDone;
-    todoExists.save();
-    return res
-      .status(200)
-      .json(
-        `${todoExists.description} marked as ${
-          todoExists.isDone ? "completed" : "not completed"
-        }`
-      );
+router.put(
+  "/update",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const { todoId } = req.body;
+    const todoExists = await todoModel.findOne({ _id: todoId });
+    if (todoExists) {
+      todoExists.isDone = !todoExists.isDone;
+      todoExists.save();
+      return res
+        .status(200)
+        .json(
+          `${todoExists.title} marked as ${
+            todoExists.isDone ? "completed" : "not completed"
+          }`
+        );
+    }
+    return res.json({ message: "todo not found" });
   }
-  return res.json({ message: "todo not found" });
-});
+);
 
 //edits title and description
-router.put("/edit", verifyJsonWebToken, async (req, res) => {
-  const { todoId, title, description } = req.body;
-  const todoExists = await todoModel.findOne({ _id: todoId });
-  if (todoExists) {
-    todoExists.title = title;
-    todoExists.description = description;
-    todoExists.save();
-    return res.status(200).json(todoExists);
+router.put(
+  "/edit",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const { todoId, title } = req.body;
+    const todoExists = await todoModel.findOne({ _id: todoId });
+    if (todoExists) {
+      todoExists.title = title;
+      todoExists.save();
+      return res.status(200).json(todoExists);
+    }
+    return res.json({ message: "todo not found" });
   }
-  return res.json({ message: "todo not found" });
-});
+);
 
 //gets specific todo
-router.get("/get", verifyJsonWebToken, async (req, res) => {
-  const { todoId } = req.body;
-  const todoExists = await todoModel.findOne({ _id: todoId });
-  if (todoExists) {
-    return res.status(200).json(todoExists);
+router.get(
+  "/get",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const { todoId } = req.body;
+    const todoExists = await todoModel.findOne({ _id: todoId });
+    if (todoExists) {
+      return res.status(200).json(todoExists);
+    }
+    return res.json({ message: "todo not found" });
   }
-  return res.json({ message: "todo not found" });
-});
+);
 
 //gets all tasks
-router.get("/all", verifyJsonWebToken, async (req, res) => {
-  const { userId } = req.id;
-  const alltasks = [];
-  (await todoModel.find({ userId })).forEach((each) => {
-    alltasks.push({ title: each.title, description: each.description });
-  });
-  return res.status(201).json(alltasks);
-});
+router.get(
+  "/all",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const { userId } = req.id;
+    const alltasks: TitleList[] = [];
+    (await todoModel.find({ userId })).forEach((each) => {
+      alltasks.push({ title: each.title });
+    });
+    return res.status(201).json(alltasks);
+  }
+);
 
 //gets all tasks for current date
-router.get("/today", verifyJsonWebToken, async (req, res) => {
-  const { userId } = req.id;
+router.get(
+  "/today",
+  verifyJsonWebToken,
+  async (req: Request, res: Response): Promise<any> => {
+    const { userId } = req.id;
 
-  // Get today's date at 00:00:00 and end of today at 23:59:59
-  const startOfToday = new Date();
-  startOfToday.setHours(0, 0, 0, 0); // Set to the start of the day (00:00:00)
+    // Get today's date at 00:00:00 and end of today at 23:59:59
+    const startOfToday = new Date();
+    startOfToday.setHours(0, 0, 0, 0); // Set to the start of the day (00:00:00)
 
-  const endOfToday = new Date();
-  endOfToday.setHours(23, 59, 59, 999); // Set to the end of the day (23:59:59)
+    const endOfToday = new Date();
+    endOfToday.setHours(23, 59, 59, 999); // Set to the end of the day (23:59:59)
 
-  // Fetch tasks created today for the given userId
-  const todayTasks = await todoModel.find({
-    userId,
-    createdAt: {
-      $gte: startOfToday, // greater than or equal to the start of today
-      $lte: endOfToday, // less than or equal to the end of today
-    },
-  });
+    // Fetch tasks created today for the given userId
+    const todayTasks = await todoModel.find({
+      userId,
+      createdAt: {
+        $gte: startOfToday, // greater than or equal to the start of today
+        $lte: endOfToday, // less than or equal to the end of today
+      },
+    });
 
-  // Return the filtered tasks
-  const tasks = todayTasks.map((each) => ({
-    title: each.title,
-    description: each.description,
-  }));
+    // Return the filtered tasks
+    const tasks = todayTasks.map((each) => ({
+      title: each.title,
+    }));
 
-  return res.status(200).json(tasks);
-});
+    return res.status(200).json(tasks);
+  }
+);
 
 //gets all completed tasks
-router.get("/completed", verifyJsonWebToken, async (req, res) => {
-  const { userId } = req.id;
-  const completedtasks = [];
-  (await todoModel.find({ userId, isDone: true })).forEach((each) => {
-    completedtasks.push({
-      title: each.title,
-      description: each.description,
-      isDone: each.isDone,
+router.get(
+  "/completed",
+  verifyJsonWebToken,
+  async (req: Request, res: Response): Promise<any> => {
+    const { userId } = req.id;
+    const completedtasks: Tasks[] = [];
+    (await todoModel.find({ userId, isDone: true })).forEach((each) => {
+      completedtasks.push({
+        title: each.title,
+        isDone: each.isDone,
+      });
     });
-  });
-  return res.status(201).json(completedtasks);
-});
+    return res.status(201).json(completedtasks);
+  }
+);
 
 //gets all incomplete tasks
-router.get("/incomplete", verifyJsonWebToken, async (req, res) => {
-  const { userId } = req.id;
-  const incompletetasks = [];
-  (await todoModel.find({ userId, isDone: false })).forEach((each) => {
-    incompletetasks.push({
-      title: each.title,
-      description: each.description,
-      isDone: each.isDone,
+router.get(
+  "/incomplete",
+  verifyJsonWebToken,
+  async (req: Request, res: Response): Promise<any> => {
+    const { userId } = req.id;
+    const incompletetasks: Tasks[] = [];
+    (await todoModel.find({ userId, isDone: false })).forEach((each) => {
+      incompletetasks.push({
+        title: each.title,
+        isDone: each.isDone,
+      });
     });
-  });
-  return res.status(201).json(incompletetasks);
-});
+    return res.status(201).json(incompletetasks);
+  }
+);
 
 //delete one todo
-router.delete("/delete", verifyJsonWebToken, async (req, res) => {
-  const { todoId } = req.body;
-  const todoExists = await todoModel.findByIdAndDelete({ _id: todoId });
-  if (!todoExists) {
-    return res.status(200).json({ message: "todo deleted successfully" });
+router.delete(
+  "/delete",
+  verifyJsonWebToken,
+  async (req: Request<{}, {}, Todo, {}>, res: Response): Promise<any> => {
+    const { todoId } = req.body;
+    const todoExists = await todoModel.findByIdAndDelete({ _id: todoId });
+    if (!todoExists) {
+      return res.status(200).json({ message: "todo deleted successfully" });
+    }
+    return res.json({ message: "todo not found" });
   }
-  return res.json({ message: "todo not found" });
-});
+);
 
 export { router };
---
+++ types/todo.ts
@@ -0,0 +1,11 @@
+export interface Todo {
+  todoId: string;
+  title: string;
+}
+export interface TitleList {
+  title: string;
+}
+export interface Tasks {
+  title: string;
+  isDone: boolean;
+}
